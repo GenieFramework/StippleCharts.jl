3761a8965f317aea625fa9a607f66f38f2d24caa
Adrian Salceanu <essenciary@users.noreply.github.com>
2020-10-11 11:45:01 +0200

    v0.3

--- src/StippleCharts.jl
+++ src/StippleCharts.jl
@@ -2,6 +2,7 @@ module StippleCharts
 
 import Genie
 import Stipple
+using Stipple.Reexport
 
 #===#
 
@@ -31,6 +32,7 @@ end
 #===#
 
 include("Charts.jl")
+@reexport using .Charts
 
 function __init__()
   push!(Stipple.DEPS, deps)
