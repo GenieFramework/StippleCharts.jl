9995027aaaa64bcf417d1e5928d3e1c09a879e68
Adrian Salceanu <e@essenciary.com>
2021-10-22 15:20:49 +0200

    CDN/Assets support

--- src/StippleCharts.jl
+++ src/StippleCharts.jl
@@ -10,22 +10,24 @@ const COMPONENTS = [:apexchart => :VueApexCharts]
 
 #===#
 
+const assets_config = Genie.Assets.AssetsConfig(package = "StippleCharts.jl")
+
 function deps() :: String
-  Genie.Router.route("/js/stipple/apexcharts.min.js") do
+  Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="apexcharts.min.js")) do
     Genie.Renderer.WebRenderable(
-      read(joinpath(@__DIR__, "..", "files", "js", "apexcharts.min.js"), String),
+      Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="apexcharts.min.js")),
       :javascript) |> Genie.Renderer.respond
   end
 
-  Genie.Router.route("/js/stipple/vue-apexcharts.min.js") do
+  Genie.Router.route(Genie.Assets.asset_path(assets_config, :js, file="vue-apexcharts.min.js")) do
     Genie.Renderer.WebRenderable(
-      read(joinpath(@__DIR__, "..", "files", "js", "vue-apexcharts.min.js"), String),
+      Genie.Assets.embedded(Genie.Assets.asset_file(cwd=normpath(joinpath(@__DIR__, "..")), file="vue-apexcharts.min.js")),
       :javascript) |> Genie.Renderer.respond
   end
 
   string(
-    Genie.Renderer.Html.script(src="$(Genie.config.base_path)js/stipple/apexcharts.min.js"),
-    Genie.Renderer.Html.script(src="$(Genie.config.base_path)js/stipple/vue-apexcharts.min.js")
+    Genie.Renderer.Html.script(src="$(Genie.Assets.asset_path(assets_config, :js, file="apexcharts.min.js"))"),
+    Genie.Renderer.Html.script(src="$(Genie.Assets.asset_path(assets_config, :js, file="vue-apexcharts.min.js"))")
   )
 end
 
